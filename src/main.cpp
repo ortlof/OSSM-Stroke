a7f0fb1c56893801b5e1b3081bafa27fd2e6278f
Ortlof <sm@mail4u2.de>
2022-03-13 01:53:42 +0100

    OSSM_ESP_Remote DEV

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "FastLED.h"          // Used for the LED on the Reference Board (or any other pixel LEDS you may add)
 #include "RotaryEncoder.h"
 #include "OssmUi.h"           // Separate file that helps contain the OLED screen functions for Local Remotes
+#include <esp_now.h>
+#include <WiFi.h>
 
 #define BTN_NONE   0
 #define BTN_SHORT  1
@@ -15,6 +17,45 @@
 
 volatile float speedPercentage = 0;
 volatile float sensation = 0;
+
+uint8_t broadcastAddress[] = {0x08, 0x3A, 0xF2, 0x68, 0x1E, 0x74};
+
+// Variable to store if sending data was successful
+String success;
+
+float out_esp_speed;
+float out_esp_depth;
+float out_esp_stroke;
+float out_esp_sensation;
+float out_esp_pattern;
+bool out_esp_rstate;
+bool out_esp_connected;
+
+float incoming_esp_speed;
+float incoming_esp_depth;
+float incoming_esp_stroke;
+float incoming_esp_sensation;
+float incoming_esp_pattern;
+bool incoming_esp_rstate;
+bool incoming_esp_connected;
+
+typedef struct struct_message {
+  float esp_speed;
+  float esp_depth;
+  float esp_stroke;
+  float esp_sensation;
+  float esp_pattern;
+  bool esp_rstate;
+  bool esp_connected;
+} struct_message;
+
+bool esp_connect = false;
+
+struct_message outgoingcontrol;
+struct_message incomingcontrol;
+
+esp_now_peer_info_t peerInfo;
+
 
 ///////////////////////////////////////////
 ////
@@ -29,7 +70,7 @@ volatile float sensation = 0;
 float speed     = 0.0;
 float depth     = 0.0;
 float stroke    = 0.0;
-float zero = 0.0;
+float zero      = 0.0;
 int pattern = 0;
 
 RotaryEncoder *encoder;
@@ -37,8 +78,6 @@ RotaryEncoder *encoder;
 // State Machine Local Remote
 enum States {START, HOME, M_MENUE, M_SET_DEPTH, M_SET_STROKE, M_SET_SENSATION, M_SET_PATTERN, M_SET_DEPTH_INT, M_SET_DEPTH_FANCY, OPT_SET_DEPTH, OPT_SET_STROKE, OPT_SET_SENSATION, OPT_SET_PATTERN, OPT_SET_DEPTH_INT, OPT_SET_DEPTH_FANCY};
 uint8_t state = START;
-
-
 
 
 // Display LocaL Remote
@@ -95,6 +134,7 @@ StrokeEngine Stroker;
 
 TaskHandle_t estop_T    = nullptr;  // Estop Taks for Emergency 
 TaskHandle_t CRemote_T  = nullptr;  // Cable Remote Task 
+TaskHandle_t eRemote_t  = nullptr;  // Esp Now Remote
 
 #define BRIGHTNESS 170
 #define LED_TYPE WS2811
@@ -106,6 +146,7 @@ CRGB leds[NUM_LEDS];
 // Declarations
 void emergencyStopTask(void *pvParameters); // Handels all Higher Emergency Stop Functions
 void CableRemoteTask(void *pvParameters);  // Handels all Functions from Cable Remote
+void espNowRemoteTask(void *pvParameters); // Handels the EspNow Remote
 void setLedRainbow(CRGB leds[]);
 
 float getAnalogAverage(int pinNumber, int samples);
@@ -119,6 +160,48 @@ void homingNotification(bool isHomed) {
   } else {
     g_ui.UpdateMessage("Homing failed!");
     LogDebug("Homing failed!");
+  }
+}
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingcontrol, incomingData, sizeof(incomingcontrol));
+  if(esp_connect == false && incomingcontrol.esp_connected == false){
+    outgoingcontrol.esp_connected = true;
+    outgoingcontrol.esp_speed = USER_SPEEDLIMIT;
+    outgoingcontrol.esp_depth = MAX_STROKEINMM;
+    outgoingcontrol.esp_stroke = MAX_STROKEINMM;
+    outgoingcontrol.esp_sensation = 0.0;
+    outgoingcontrol.esp_pattern = Stroker.getPattern();
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &outgoingcontrol, sizeof(outgoingcontrol));
+    if (result == ESP_OK) {
+      esp_connect = true;
+    }
+    else {
+   }
+  } else if(esp_connect == true && incomingcontrol.esp_connected == true){
+    Stroker.setSpeed(incomingcontrol.esp_speed, false);
+    Stroker.setDepth(incomingcontrol.esp_depth, false);
+    Stroker.setStroke(incomingcontrol.esp_stroke, false);
+    Stroker.setSensation(incomingcontrol.esp_sensation, false);
+    Stroker.setPattern(incomingcontrol.esp_pattern, false);
+    if(incomingcontrol.esp_rstate == true){
+      Stroker.startPattern();
+    } else {
+     Stroker.stopMotion();
+    }
   }
 }
 
@@ -157,10 +240,23 @@ void setup() {
                             &CRemote_T,         /* Task handle to keep track of created task */
                             0);                 /* pin task to core 0 */
   delay(100);
+  
+  xTaskCreatePinnedToCore(espNowRemoteTask,      /* Task function. */
+                            "espNowRemoteTask",  /* name of task. */
+                            4096,               /* Stack size of task */
+                            NULL,               /* parameter of the task */
+                            5,                  /* priority of the task */
+                            &eRemote_t,         /* Task handle to keep track of created task */
+                            0);                 /* pin task to core 0 */
+  delay(100);
+  
+
   if(!g_ui.DisplayIsConnected()){
     vTaskSuspend(CRemote_T);
   }
   
+
+
   pinMode(SERVO_ALM_PIN, INPUT);
   pinMode(SERVO_PED_PIN, INPUT);
 
@@ -204,6 +300,9 @@ void emergencyStopTask(void *pvParameters)
         }
         vTaskSuspend(CRemote_T);
 
+    }
+    if(esp_connect == true){
+      vTaskSuspend(CRemote_T);
     }
     vTaskDelay(200);
   }
@@ -511,6 +610,42 @@ void CableRemoteTask(void *pvParameters)
      Stroker.setSpeed(speed, true);
      vTaskDelay(100);
    }
+}
+
+void espNowRemoteTask(void *pvParameters)
+{
+    WiFi.mode(WIFI_STA);
+    LogDebug(WiFi.macAddress());
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+    }
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(OnDataSent);
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+  
+      // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    vTaskSuspend(eRemote_t);
+    return;
+    } else {
+      vTaskSuspend(CRemote_T);
+    }
+    // Register for a callback function that will be called when data is received
+    esp_now_register_recv_cb(OnDataRecv);
+
+    for(;;)
+    {
+      vTaskDelay(500);
+    }
 }
 
 float getAnalogAverage(int pinNumber, int samples)
